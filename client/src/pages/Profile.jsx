0ced9c1bec3bb36dcb493aea5ad03f86d759a672
Constance <constance@MacBook-Pro-de-Constance.local>
2021-11-12 16:55:17 +0100

    github fix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import MyLikedJournies from './MyLikedJournies';
+import MyOwnJournies from './MyOwnJournies';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   return (
     <div>
-      profile
+      <div>infos du profile</div>
+
+      <MyLikedJournies />  OR
+      <MyOwnJournies />
+
+      On va mapper sur tout l'array de journey qu'on aura
+      <Link to="./journey"> A journey (for now) </Link>
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="./createSearchJourney">Create a journey</NavLink>
+      </div>
     </div>
   )
 }
