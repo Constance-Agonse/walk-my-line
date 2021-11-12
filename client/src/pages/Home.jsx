7df0ea11fdc0b57aeb158c2d5cf1eb80ac9ab0ae
Hugo Lansade <lhugo@lansade.com>
2021-11-12 16:21:17 +0100

    add a link to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       c'est la home
+      <NavLink exact to="/profile">Profile</NavLink>
+      <NavLink exact to="/createSearchJourney">Create a journeyyy !</NavLink>
+
     </div>
   )
 }
